c38f434f7bf1a3d793d85f20de572353f438ab9a
Rainiver <101853717+Rainiver@users.noreply.github.com>
2025-09-18 22:52:36 +0800

    LC-search-a-2d-matrix [Runtime: N/A; Memory: N/A]

---
+++ matrix/search-a-2d-matrix.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        """
+        :type matrix: List[List[int]]
+        :type target: int
+        :rtype: bool
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        l = 0
+        r = m * n - 1
+        while l <= r:
+            mid = (l+r)//2
+            if matrix[mid//n][mid%n] < target:
+                l = mid + 1
+            elif matrix[mid//n][mid%n] == target:
+                return True
+            else:
+                r = mid -1
+            
+        return matrix[l//n][l%n] == target
+
+        
