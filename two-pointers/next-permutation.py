25e801148e0d4bcc61842e7a78c8e7edd359d865
Rainiver <rainiver703@gmail.com>
2025-09-20 11:33:57 +0800

    LC-next-permutation [Runtime: N/A; Memory: N/A]

---
+++ two-pointers/next-permutation.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def nextPermutation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        pivot = -1
+        n = len(nums)
+        for i in range(n-1, 0, -1):
+            if nums[i-1] < nums[i]:
+                pivot = i - 1
+                break
+        if pivot >= 0 :
+            for i in range(n-1, pivot, -1):
+                if nums[i] > nums[pivot]:
+                    nums[i], nums[pivot] = nums[pivot], nums[i]
+
+        l, r = pivot + 1, n-1
+        while l < r:
+           nums[l], nums[r] = nums[r], nums[l]  
+           l += 1
+           r -= 1
+        
+        return nums
