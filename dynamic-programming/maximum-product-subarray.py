d325456aa45ff1cac55228e6cd0bbf974415fafc
Rainiver <rainiver703@gmail.com>
2025-09-20 18:02:54 +0800

    LC-maximum-product-subarray [Runtime: 11 ms; Memory: 12.7 MB]

--- dynamic-programming/maximum-product-subarray.py
+++ dynamic-programming/maximum-product-subarray.py
@@ -4,12 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # dp[i] 考虑下标i以及之前的最大product
-        n = len(nums)
-        dp = nums
-        # dp[0] = nums[0]
-        ans = dp[0]
-        for i in range(1, n):
-            dp[i] = max(dp[i], dp[i-1]*nums[i])
-            ans = max(ans, dp[i])
+        # 由于存在负负得正，需要维护两个变量，分别为最大乘积和最小乘积
+        # 计算最大乘积和最小乘积*当前元素的值，更新最大乘积和最小乘积。使用最大乘积更新结果
+        # n = len(nums)
+        ans = nums[0]
+        max_pro, min_pro = ans, ans
+        # 从第二个元素开始遍历（跳过已经初始化的第一个元素）
+        for num in nums[1:]:
+            pre_max, pre_min = max_pro, min_pro
+            max_pro = max(num, pre_max*num, pre_min*num)
+            min_pro = min(num, pre_max*num, pre_min*num)
+            ans = max(ans, max_pro)
         return ans
