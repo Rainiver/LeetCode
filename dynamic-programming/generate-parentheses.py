587078f59751d6e2f7f7bdcccbf6aae2f2238b9a
Rainiver <rainiver703@gmail.com>
2025-09-21 15:57:08 +0800

    LC-generate-parentheses [Runtime: 2 ms; Memory: 12.7 MB]

---
+++ dynamic-programming/generate-parentheses.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        ans = []
+        
+        def dfs(l, r, path):
+            # 递归终止条件
+            if l == n and r == n:
+                ans.append(path)
+                return
+            # 左括号都能加
+            if l < n:
+                dfs(l+1, r, path+"(")
+                # path自动恢复为原来的，无需手动回溯
+            if  r < l:
+                dfs(l, r+1, path+")")
+                # path自动恢复为原来的，无需手动回溯
+
+        dfs(0, 0,  "")
+        return ans
+        
