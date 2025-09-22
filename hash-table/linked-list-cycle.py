73149a2d057a6925fdaf117dfd373189e7e7c569
Rainiver <rainiver703@gmail.com>
2025-09-22 16:55:18 +0800

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

--- hash-table/linked-list-cycle.py
+++ hash-table/linked-list-cycle.py
@@ -11,7 +11,7 @@ class Solution(object):
         :rtype: bool
         """
         fast, slow = head, head
-        while fast.next:  
+        while slow.next:  
             fast = fast.next.next
             slow = slow.next
             if fast == slow:
