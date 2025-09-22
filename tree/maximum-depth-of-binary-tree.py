93c8822ffcc7f2609d48e371804b3096a744d6b5
Rainiver <rainiver703@gmail.com>
2025-09-23 01:10:22 +0800

    LC-maximum-depth-of-binary-tree [Runtime: 1 ms; Memory: 15.1 MB]

--- tree/maximum-depth-of-binary-tree.py
+++ tree/maximum-depth-of-binary-tree.py
@@ -17,14 +17,14 @@ class Solution(object):
         queue = [root]
         while queue:
             level_size = len(queue) # 记录当前层节点数量，处理完当前层depth才+1
-            node = queue.pop(0)
-            level_size -= 1
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-            if level_size == 0:
-                depth += 1
+            for i in range(level_size):
+                node = queue.pop(0)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            # 处理完一层，深度+1
+            depth += 1
         return depth
 
 
