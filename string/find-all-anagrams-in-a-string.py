b4bd1151e75842234142c3b03d1315fb872a1ab6
Rainiver <rainiver703@gmail.com>
2025-09-22 18:21:34 +0800

    LC-find-all-anagrams-in-a-string [Runtime: 39 ms; Memory: 13.3 MB]

--- string/find-all-anagrams-in-a-string.py
+++ string/find-all-anagrams-in-a-string.py
@@ -14,7 +14,7 @@ class Solution(object):
         l, r = 0, n_p-1
         n = len(s)
         if n_p > n:
-            return
+            return ans
         for i in range(n_p):
             s_count[ord(s[i])-ord("a")] += 1
         if s_count == p_count:
