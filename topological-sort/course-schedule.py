f2a7f3f8e958f533b94e73cc09d4c004f3032c22
Rainiver <rainiver703@gmail.com>
2025-09-23 00:22:17 +0800

    LC-course-schedule [Runtime: N/A; Memory: N/A]

--- topological-sort/course-schedule.py
+++ topological-sort/course-schedule.py
@@ -22,12 +22,12 @@ class Solution(object):
         # 对这些节点，找到邻接节点，入度-1，如果减到0则加入队列
         while queue:
             for item in queue: # 队列中的每个节点
-                for i in adj[item]: # 节点的邻接点是一个列表呀
+                for i in adj[item]: # 节点的邻接点是一个列表
                     in_degree[i] -= 1
                     if in_degree[i] == 0:
                         queue.append(i)
                 
-            queue.pop(-1)
+            queue.pop(0)
         # 如果in_degree每个值都为0，则true，否则false
         for i in range(len(in_degree)):
             if in_degree[i] > 0:
