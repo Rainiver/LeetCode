329c903c7e9df50778ffc1aa8f37f417fd72b1d5
Rainiver <rainiver703@gmail.com>
2025-09-23 13:47:06 +0800

    LC-climbing-stairs [Runtime: 0 ms; Memory: 12.3 MB]

--- math/climbing-stairs.py
+++ math/climbing-stairs.py
@@ -7,6 +7,8 @@ class Solution(object):
         # dp[i]表示第i步时的方法
         # dp[i] = dp[i-1]+dp[i-2]
         dp = [0] * (n+1)
+        if n == 1:
+            return 1
         dp[1] = 1
         dp[2] = 2
         for i in range(3, n+1):
