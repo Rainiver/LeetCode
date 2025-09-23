2c77a99fee0baece3c6688af3a65f545e809c8d7
Rainiver <rainiver703@gmail.com>
2025-09-23 14:27:44 +0800

    LC-merge-two-sorted-lists [Runtime: 0 ms; Memory: 12.6 MB]

---
+++ linked-list/merge-two-sorted-lists.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        p1, p2, cur = list1, list2, dummy # cur 指向当前合并节点的尾部
+        while p1 and p2:
+            if p1.val <= p2.val:
+                cur.next = p1
+                p1 = p1.next
+            else:
+                cur.next = p2
+                p2 = p2.next
+            cur = cur.next
+            
+        
+        if p1:
+            cur.next = p1
+        elif p2:
+            cur.next = p2
+        # 不知道怎么返回头节点
+        return dummy.next
