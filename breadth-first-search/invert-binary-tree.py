2d232e88548010ddc87d436e7667e23dc74dd5ec
Rainiver <rainiver703@gmail.com>
2025-09-23 16:19:59 +0800

    LC-invert-binary-tree [Runtime: N/A; Memory: N/A]

---
+++ breadth-first-search/invert-binary-tree.py
@@ -0,0 +1,30 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return root
+
+        def dfs(node, left, right):
+            if not node.left or not node.right:
+                return
+            
+            # 交换左右节点，如何实现交换左右子树？
+            node.left, node.right = node.right, node.left
+            if node.left:
+                tmp = node.left
+                dfs(tmp, tmp.left, tmp.right)
+            if node.right:
+                tmp = node.right
+                dfs(tmp, tmp.left, tmp.right)
+
+        dfs(root, root.left, root.right)
+        return root
