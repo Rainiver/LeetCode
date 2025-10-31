1e20c969a0e1d2cf702ab46bb74c3fff20d09c61
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2025-10-31 18:03:58 +0000

    LC-merge-intervals [Runtime: N/A; Memory: N/A]

--- array/merge-intervals.py
+++ array/merge-intervals.py
@@ -4,21 +4,18 @@ class Solution(object):
         :type intervals: List[List[int]]
         :rtype: List[List[int]]
         """
-
-        intervals.sort(key = lambda x:x[0])
         ans = []
         n = len(intervals)
-        l = intervals[0][0]
-        r = intervals[0][1]
+        if n == 1:
+            return intervals
+        intervals.sort(key=lambda x:x[0])
+        l, r = intervals[0][0], intervals[0][1]
         for i in range(1, n):
-            # 有重叠部分，合并区间
             if intervals[i][0] <= r:
-                r = max(r, intervals[i][1])
-            # 无重叠部分，左右边界加入答案并更新
+                r = intervals[i][1]
             else:
-                ans.append([l,r])
+                ans.append([l, r])
                 l = intervals[i][0]
                 r = intervals[i][1]
-        ans.append([l,r])
+        ans.append([l, r])
         return ans
-        
