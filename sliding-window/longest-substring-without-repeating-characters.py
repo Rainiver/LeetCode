6ed6463b6c9b2ff1b2855b71572268bd7b708193
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2025-10-31 23:53:45 +0000

    LC-longest-substring-without-repeating-characters [Runtime: N/A; Memory: N/A]

--- sliding-window/longest-substring-without-repeating-characters.py
+++ sliding-window/longest-substring-without-repeating-characters.py
@@ -4,13 +4,15 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        ans = 0
-        pos = [-1]*128
+
         l, r = 0, 0
+        ans = 0
+        pos = [0] * 128
         for char in s:
-            char_code = ord(char)
-            l = max(l, pos[char_code] + 1)
-            ans = max(ans, r - l + 1)
-            pos[char_code] = r
+            char_ord = ord(char)
+            
+            l = max(l, pos[char_ord])
+            pos[char_ord] = r
+            ans = max(ans, r - l)
             r += 1
         return ans
