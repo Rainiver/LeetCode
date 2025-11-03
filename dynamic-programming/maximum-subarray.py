f70e7479a4be259550eda704bda944d92d6ac88a
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2025-11-03 14:26:30 +0000

    LC-maximum-subarray [Runtime: 71 ms; Memory: 32.6 MB]

--- dynamic-programming/maximum-subarray.py
+++ dynamic-programming/maximum-subarray.py
@@ -1,16 +1,11 @@
-class Solution(object):
-    def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        # 延续前面的子序列 or 从这里从头开始计算
-        # dp[i] = max(nums[i], dp[i-1]+nums[i])
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [0] * n
+        if n == 1:
+            return nums[0]
+        dp = [0]*n
         dp[0] = nums[0]
-        ans = dp[0]
         for i in range(1, n):
-            dp[i] = max(nums[i], dp[i-1] + nums[i])
-            ans = max(ans, dp[i])
-        return ans
+            dp[i] = max(nums[i], dp[i-1]+nums[i])
+
+        return max(dp)
