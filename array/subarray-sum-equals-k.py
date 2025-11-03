274875ab1d980e1c2b84704166c549e1da1b13b3
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2025-11-03 14:47:34 +0000

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

---
+++ array/subarray-sum-equals-k.py
@@ -0,0 +1,17 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        prefix_sum = [0]*(n+1)
+        pre_sum = 0
+        if n == 1:
+            return 1
+        for i in range(n):
+            prefix_sum[i+1] = prefix_sum[i]+nums[i]
+
+        count = 0
+        for i in range(n):
+            for j in range(i+1, n+1):
+                if k == prefix_sum[j] - prefix_sum[i]:
+                    count += 1
+        
+        return count
