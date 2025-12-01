eecd5561785e7f1272755def389fe27ce6e6c5c9
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2025-12-01 11:43:46 +0000

    LC-reorganize-string [Runtime: N/A; Memory: N/A]

---
+++ greedy/reorganize-string.py
@@ -0,0 +1,37 @@
+import heapq
+from collections import deque
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        # n = 1 的task schedular
+        hashmap = {}
+        heap = []
+        queue = deque()
+        res = ""
+        
+        for char in s:
+            if char not in hashmap:
+                hashmap[char] = 1
+            else:
+                hashmap[char] += 1
+
+        for key, value in hashmap.items():
+            heapq.heappush(heap, (key, -value)) 
+        time = 0
+        while heap or queue:
+            time += 1
+            flag = False
+            while queue and queue[0][2] == time:
+                char, freq, t = queue.popleft()
+                heapq.heappush(heap, (char, freq))
+                flag = True
+            if heap:
+                char, freq = heapq.heappop(heap)
+                res += char
+                freq += 1
+                if freq < 0:
+                    queue.append((char, freq, time + 2))
+                flag = True
+            if not flag:
+                return ""
+
+        return res
