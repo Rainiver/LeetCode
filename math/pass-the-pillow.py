2f2fdb7d71760678c6d45171c69c332404b44f0c
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2025-12-06 11:23:20 +0000

    LC-pass-the-pillow [Runtime: N/A; Memory: N/A]

---
+++ math/pass-the-pillow.py
@@ -0,0 +1,11 @@
+class Solution:
+    def passThePillow(self, n: int, time: int) -> int:
+        cycle = 2* (n-1)
+        ans = time % cycle
+        if ans < n:
+            return ans + 1
+        else:
+            return 2 * (n-1) - n
+
+
+        
