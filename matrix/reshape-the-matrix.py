90798b7e517069b63d425452e06ca7023630b11b
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2025-12-06 10:55:55 +0000

    LC-reshape-the-matrix [Runtime: N/A; Memory: N/A]

---
+++ matrix/reshape-the-matrix.py
@@ -0,0 +1,15 @@
+class Solution:
+    def matrixReshape(self, mat: List[List[int]], r: int, c: int) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        ans = [[0]*c for _ in range(r)]
+        for i in range(m):
+            for j in range(n):
+                idx = n * i + j
+                row = idx // c
+                col = idx % c
+                ans[row][col] = mat[i][j]
+        
+        return ans if r * c == m * n else mat
+        
+        
