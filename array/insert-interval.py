b0a38054101d0e974ef2504bb50956ab5c9060b2
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2026-01-26 08:52:58 +0000

    LC-insert-interval [Runtime: 0 ms; Memory: 21.6 MB]

--- array/insert-interval.py
+++ array/insert-interval.py
@@ -6,16 +6,14 @@ class Solution:
         res = []
         l, r = intervals[0][0], intervals[0][1]
         for i in range(1, n):
-            if l <= intervals[i][0] <= r and intervals[i][1] >= r:
-                r = intervals[i][1]
-            elif l <= intervals[i][0] <= r and intervals[i][1] < r:
-                continue
+            if l <= intervals[i][0] <= r :
+                r = max(r, intervals[i][1])
             else:
                 res.append([l, r])
                 l = intervals[i][0]
                 r = intervals[i][1]
-        if l == intervals[n-1][0] and r == intervals[n-1][1]:
-            res.append([l, r])
+        
+        res.append([l, r])
         return res
 
         
