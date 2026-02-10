2bf21b169539b010ba33fb3809d626d0ddf1db49
Rainie (Yuhe) Zhong <rainiver703@gmail.com>
2026-02-10 16:18:23 +0000

    LC-maximum-depth-of-binary-tree [Runtime: N/A; Memory: N/A]

--- binary-tree/maximum-depth-of-binary-tree.py
+++ binary-tree/maximum-depth-of-binary-tree.py
@@ -1,31 +1,24 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def maxDepth(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: int
-        """
-        if not root:
-            return 0
-
-        depth = 0
-        queue = [root]
+from collections import deque
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        queue = deque([root])
+        res = 0
         while queue:
-            level_size = len(queue) # 记录当前层节点数量，处理完当前层depth才+1
-            for i in range(level_size):
-                node = queue.pop(0)
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            # 处理完一层，深度+1
-            depth += 1
-        return depth
-
-
+            level = []
+            for i in range(len(queue)):
+                curr = queue.popleft()
+                level.append(curr.val)
+                if curr.left:
+                    queue.append(curr.left)
+                if curr.right:
+                    queue.append(curr.right)
+            if len(level) > 0:
+                res += 1
+        return res
         
